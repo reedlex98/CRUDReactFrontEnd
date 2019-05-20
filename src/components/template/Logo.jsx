69781d71bf3546c42b6f570667d09140a01c3697
Elder <elder_louzada@outlook.com>
2019-05-20 16:22:26 -0300

    Logo component style fnished | logo.png was added

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -1,8 +1,11 @@
 import './Logo.css'
 import React from 'react'
+import logo from '../../assets/imgs/logo.png'
 
 export default props => {
     return <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo"/>
+        </a>
     </aside>
 }
