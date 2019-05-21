e72b70f51d6821c6d584c290f4480b3a96e37768
Elder <elder_louzada@outlook.com>
2019-05-21 17:36:15 -0300

    Header component style finished, bootstrap added

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 
 import Logo from '../components/template/Logo'
@@ -10,6 +12,7 @@ import React from 'react'
 export default props => <div className="app">
     <Logo/>
     <Nav/>
-    <Main/>
+    <Main icon="home" title="Inicio"
+        subtitle="Second project, React Course" />
     <Footer/>   
 </div>
