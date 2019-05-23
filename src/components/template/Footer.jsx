4187f46001d56f6c286771467ad19b237a21f623
Elder <elder_louzada@outlook.com>
2019-05-23 14:59:36 -0300

    footer component style was finished

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -1,5 +1,9 @@
 import './Footer.css'
 import React from 'react'
 
-export default props => <footer className="footer">
-Footer</footer>
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Reedlex98</strong>
+        </span>
+    </footer>
