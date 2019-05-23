95604ed143e9faeb20a2a2f49516e741ecf69958
Elder <elder_louzada@outlook.com>
2019-05-23 15:39:54 -0300

    Home component add and styled

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Home"
+        subtitle="Second project, React Course">
+        <div className="display-4">Welcome</div>
+        <hr />
+        <p className="mb-0">
+            It's a system made to exemplify the building process of a register CRUD app developed using React
+    </p>
+    </Main>
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -3,16 +3,15 @@ import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 
 import Logo from '../components/template/Logo'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Nav from '../components/template/Nav'
 import Footer from '../components/template/Footer'
 
 import React from 'react'
 
 export default props => <div className="app">
-    <Logo/>
-    <Nav/>
-    <Main icon="home" title="Inicio"
-        subtitle="Second project, React Course" />
-    <Footer/>   
+    <Logo />
+    <Nav />
+    <Home/>
+    <Footer />
 </div>
