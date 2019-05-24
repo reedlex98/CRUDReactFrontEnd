6a50ced3df58711e753737d22d38402c2027d332
Elder <elder_louzada@outlook.com>
2019-05-23 23:53:10 -0300

    Integration with backend was partially done

--- src/components/user/userCrud.jsx
+++ src/components/user/userCrud.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Main from '../template/Main'
+import axios from 'axios'
 
 const headerProps = {
     icon: 'users',
@@ -7,7 +8,41 @@ const headerProps = {
     subtitle: 'Users register: Inserting, Listing, Update and Deletion'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState ={
+    user:{
+        name: '',
+        email:''
+    },
+    list:[]
+}
+
 export default class UserCrud extends Component {
+    state = {...initialState}
+
+    clear(){
+        this.setState({ user: initialState.user })
+    }
+
+    save(){
+        //to include a new information I have to use HTTP Post method
+        //to alter/update an existing information I ve to use HTTP Put/Patch method
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp =>{
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState, list})
+            })
+    }
+
+    getUpdatedList(user){
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}>
