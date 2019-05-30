48dfa9ddeffbef88338d124d397199092cba8cf8
Elder <elder_louzada@outlook.com>
2019-05-30 15:20:31 -0300

    Updates

---
+++ precache-manifest.c09c9d5e052a5bc3d04fd01690d7a688.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182fff6f3cff68c80d6c658ae53ec77d",
+    "url": "/CRUDReactFrontEnd/index.html"
+  },
+  {
+    "revision": "d282b22ddda133c9b27d",
+    "url": "/CRUDReactFrontEnd/static/css/2.34db7945.chunk.css"
+  },
+  {
+    "revision": "25f5bb902a53abc593fe",
+    "url": "/CRUDReactFrontEnd/static/css/main.59e1b5d9.chunk.css"
+  },
+  {
+    "revision": "d282b22ddda133c9b27d",
+    "url": "/CRUDReactFrontEnd/static/js/2.7ebfb67d.chunk.js"
+  },
+  {
+    "revision": "25f5bb902a53abc593fe",
+    "url": "/CRUDReactFrontEnd/static/js/main.77be2641.chunk.js"
+  },
+  {
+    "revision": "5ad8602bd5442944d44d",
+    "url": "/CRUDReactFrontEnd/static/js/runtime~main.4deacf8c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CRUDReactFrontEnd/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CRUDReactFrontEnd/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CRUDReactFrontEnd/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CRUDReactFrontEnd/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CRUDReactFrontEnd/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6777486e7ac73f627f41dceddc0eedd1",
+    "url": "/CRUDReactFrontEnd/static/media/logo.6777486e.png"
+  }
+]);
